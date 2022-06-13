6e6527005b7de077f80ac7de6cf84bd12a385c25
Hammed Babatunde <dev_babs@pop-os.localdomain>
2022-06-13 20:35:06 +0100

    guessing game project

---
+++ guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+def guess_number(x):
+    random_number = random.randint(1, x)
+
+    guess = 0
+
+    while(guess != random_number):
+        try:
+            guess = int(input(f'Guess input a number between 1 and {x}: '))
+        except ValueError:
+            print('Please enter a Valid Number')
+        else:
+            if guess < random_number:
+                print('Sorry, guessed number is too low, try again')
+            elif guess > random_number:
+                print('Sorry, guessed number is too high, try again')
+
+    print(f"Yay, you have correctly guessed {random_number}") 
+
+guess_number(20)
