7037e051d09163e1a917ca1c34e5bd1ce803c3b5
Hammed Babatunde <dev_babs@pop-os.localdomain>
2022-06-13 18:52:33 +0100

    Created and finishedmadlib project

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+country_name = input('What is the name of your country: ')
+city = input('What is the name of the city your family resides: ')
+college_name = input('What is the name of your college: ')
+
+madlib = f'The name of my country is {country_name} and my family resides in {city}. I study in {college_name}.'
+
+print(madlib)
